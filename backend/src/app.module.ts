a239848dd9817148b16e4b6960f936f1b56c37e4
Tksi <24800668+Tksi@users.noreply.github.com>
2023-08-16 00:13:46 +0900

    feat(backend): 雛形作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PostModule],
 })
 export class AppModule {}
