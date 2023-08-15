9e55b65a37ad9422eab40ec2e4bf3d97c00b35b6
Tksi <24800668+Tksi@users.noreply.github.com>
2023-08-16 00:42:53 +0900

    feat(backend): create post endpoint

--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateRequest } from './dto/create.dto';
+import { PostService } from './post.service';
+import { AuthGuard } from '@/auth/auth.guard';
 
+@ApiTags('post')
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async create(@Body() createRequest: CreateRequest, @Request() req) {
+    console.log(req);
+
+    return this.postService.create(createRequest, req.user.id);
+  }
+}
--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateRequest } from './dto/create.dto';
+import { PrismaService } from '@/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createRequest: CreateRequest, userId: number) {
+    await this.prismaService.post.create({
+      data: {
+        ...createRequest,
+        userId,
+      },
+    });
+  }
+}
