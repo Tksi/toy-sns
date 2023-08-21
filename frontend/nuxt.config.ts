ea01d8e1f66da35281dd684b0c4d5a67f5da1522
Tksi <24800668+Tksi@users.noreply.github.com>
2023-08-21 19:11:08 +0900

    chore(backend): install vuetify, vitest

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@invictus.codes/nuxt-vuetify'],
   devtools: { enabled: true },
   ssr: false,
   typescript: {
