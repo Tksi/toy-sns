76ed7830927f0954fdc3c980791a54f9a5a70589
Tksi <24800668+Tksi@users.noreply.github.com>
2023-08-25 19:49:47 +0900

    feat(frontend): del unused code

--- frontend/components/atoms/GrayLink.vue
+++ frontend/components/atoms/GrayLink.vue
@@ -2,7 +2,6 @@
   <div class="w-100" style="text-align: right">
     <NuxtLink :to="link">{{ text }}</NuxtLink>
   </div>
-  <v-btn class="mt-2" color="primary" @click="() => $router.push('/login')" />
 </template>
 
 <script setup lang="ts">
