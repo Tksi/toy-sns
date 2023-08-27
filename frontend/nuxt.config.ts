f8dcb557c1eb55c465f6f3958061ba74aad9685e
Tksi <24800668+Tksi@users.noreply.github.com>
2023-08-27 21:55:42 +0900

    feat(frontend): add favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -20,4 +20,15 @@ export default defineNuxtConfig({
       'Noto Sans JP': true,
     },
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐤</text></svg>',
+        },
+      ],
+    },
+  },
 });
